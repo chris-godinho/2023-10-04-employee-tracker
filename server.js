52fc327f1e48748e0623a73f3cd00c47f278fe87
kristbg <kristbg@gmail.com>
2023-10-05 20:28:44 -0400

    Created basic file structure, added schema

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Placeholder2023!',
+    database: ''
+  },
+  console.log(`Connected to database.`)
+);
+
+// Query database
+db.query('SELECT * FROM students', function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
